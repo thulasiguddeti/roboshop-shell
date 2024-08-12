0621e937e85fd1f8a4b94dd0f0912f72341a9001
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-08-12 11:32:45 +0530

    roboshop shell scripts

---
+++ mongodb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ...$R FAILED $N"
+    else 
+        echo -e "$2 ...$G SUCCESS $N"
+    fi   
+}
